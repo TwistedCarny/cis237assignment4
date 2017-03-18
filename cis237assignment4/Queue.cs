ac9b16f8bbf0248d193fbf6090589e0da71f830a
Westin Curtis <westincurtis@gmail.com>
2017-03-18 14:24:26 -0400

    Finish adding queue

--- cis237assignment4/Queue.cs
+++ cis237assignment4/Queue.cs
@@ -8,18 +8,69 @@ namespace cis237assignment4
 {
     class Queue<T> : IQueue<T>
     {
-        public bool IsEmpty => throw new NotImplementedException();
-
-        public int Size => throw new NotImplementedException();
-
-        public T Dequeue(T data)
+        protected class Node
         {
-            throw new NotImplementedException();
+            public T Data { get; set; }
+            public Node Next { get; set; }
+        }
+
+        protected Node _head;
+        protected Node _tail;
+        protected int _size;
+
+        public bool IsEmpty
+        {
+            get { return _head == null; }
+        }
+
+        public int Size
+        {
+            get { return _size; }
         }
 
         public void Enqueue(T data)
         {
-            throw new NotImplementedException();
+            Node oldTail = _tail;
+
+            _tail = new Node();
+
+            _tail.Data = data;
+
+            _tail.Next = null;
+
+            if (IsEmpty)
+            {
+                _head = _tail;
+            }
+            else
+            {
+                oldTail.Next = _tail;
+            }
+
+            _size++;
         }
+
+        public T Dequeue(T data)
+        {
+            if (IsEmpty)
+            {
+                throw new Exception("List is empty");
+            }
+
+            T returnData = _head.Data;
+
+            _head = _head.Next;
+
+            _size--;
+
+            if (IsEmpty)
+            {
+                _tail = null;
+            }
+
+            return returnData;
+        }
+
+        
     }
 }
